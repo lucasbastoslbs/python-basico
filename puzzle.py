e7d0026abfdd128f2ae6c8582a6a42989f9d5ff5
Lucas Bastos <lsbastos15@gmail.com>
2023-08-24 21:29:01 -0300

    puzzle update

--- puzzle.py
+++ puzzle.py
@@ -11,36 +11,32 @@ def exibir(matriz):
 def cima(l,c):
     if(l == 0):
         return
-    matriz[l][c] = matriz[l-1][c]
-    matriz[l-1][c] = 0
+    matriz[l][c], matriz[l-1][c] = matriz[l-1][c], matriz[l][c]
     return l-1,c
 
 def baixo(l,c):
     if l == len(matriz):
         return
-    matriz[l][c] = matriz[l+1][c]
-    matriz[l+1][c] = 0
+    matriz[l][c], matriz[l+1][c] = matriz[l+1][c],matriz[l][c]
     return l+1,c
 
 def esquerda(l,c):
     if(c == 0):
         return
-    matriz[l][c] = matriz[l][c-1]
-    matriz[l][c-1] = 0
+    matriz[l][c],matriz[l][c-1] = matriz[l][c-1], matriz[l][c]
     return l,c-1
 
 def direita(l,c):
     if(c == len(matriz)):
         return
-    matriz[l][c] = matriz[l][c+1]
-    matriz[l][c+1] = 0
+    matriz[l][c], matriz[l][c+1] = matriz[l][c+1], matriz[l][c]
     return l,c+1
 
 def iniciaMatriz(n):
     numeros = [ele for ele in range(n*n)]
     lista = []
-    lista = [[1,0,2],[3,4,5],[6,7,8]]
-    return lista,0,1
+    # lista = [[1,0,2],[3,4,5],[6,7,8]]
+    # return lista,0,1
     for i in range(n):
         temp = []
         for j in range(n):
@@ -57,7 +53,7 @@ def estagio_final(n):
     for i in range(n):
         temp = []
         for j in range(n):
-            temp.append(i * n + j)
+            temp.append(i*n + j)
         fim.append(temp)
     if(fim == matriz):
         return True
